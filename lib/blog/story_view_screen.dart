be60435dc9a1da00e3af919e91a62805ecc8172e
Salmahegazy32 <salmashady12355@gmail.com>
2023-07-11 03:18:04 +0300

    TEST

--- lib/blog/story_view_screen.dart
+++ lib/blog/story_view_screen.dart
@@ -13,7 +13,7 @@ class _StoryViewScreenState extends State<StoryViewScreen> {
   final controller = StoryController();
   Widget build(context) {
     List<StoryItem> storyItems = [
-      StoryItem.text( title: 'Abanob', backgroundColor: Colors.blueGrey),
+      StoryItem.text( title: 'Salma', backgroundColor: Colors.blueGrey),
       StoryItem.pageImage(url: 'https://blog.logrocket.com/wp-content/uploads/2021/05/intro-dart-flutter-feature.png', controller: controller),
       StoryItem.pageImage(url: 'https://phantom-marca.unidadeditorial.es/e78616bbda77fe6fbc6b1ed5a05d3232/resize/828/f/webp/assets/multimedia/imagenes/2023/06/21/16873550466244.png', controller: controller),
 
